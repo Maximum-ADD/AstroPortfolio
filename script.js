355a57ed86de968a688e9734c79a047bef6fc5e5
Adrian Draxl <adrianadraxl@gmail.com>
2025-09-13 15:32:52 +0200

    deleted old code
    this code is very cute but I need to get rid of it now. thank goodness for roll back.

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-  const scrollY = window.scrollY;
-
-  if (scrollY > 50) {
-    document.querySelector('.left-door').style.transform = 'translateX(-100%)';
-    document.querySelector('.right-door').style.transform = 'translateX(100%)';
-    document.querySelector('.computer-screen').style.opacity = '0';
-  }
-});
